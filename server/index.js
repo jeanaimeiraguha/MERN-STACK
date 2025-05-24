f4224e977c0e3d96219d000cd467353b10fa9ee4
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2025-05-24 13:10:33 +0200

    Mern

--- server/index.js
+++ server/index.js
@@ -2,29 +2,41 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/users", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.error("MongoDB connection error:", err));
-
-// Import user model
-const userModel = require('./model/users');
-
-// Create a new user
-app.post("/create", (req, res) => {
-  userModel.create(req.body)
-    .then(user => res.json(user))
-    .catch(err => res.status(500).json({ error: err.message }));
+const StudentsSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number
 });
 
-// Start the server
+const Students = mongoose.model('Students', StudentsSchema);
+app.post('/user/addNew', (req, res) => {
+  const { name, email, age } = req.body;
+  if (!name || !email || !age) {
+    return res.status(400).json({ error: "All fields are required: name, email, age" });
+  }
+  
+  User.create({ name, email, age })
+    .then(newUser => {
+      res.status(201).json({ message: "User added successfully", user: newUser });
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+mongoose.connect(
+  "mongodb+srv://jeanaimeiraguha:Iraguha@mern.sazoqkg.mongodb.net/School?retryWrites=true&w=majority&appName=Mern"
+)
+.then(() => console.log("MongoDB connected successfully"))
+.catch(err => console.error(" MongoDB connection error:", err));
+
+app.get('/', (req, res) => {
+  res.send("Server is running and connected to MongoDB");
+});
+
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log('Server running on http://localhost:3000');
 });
--- server/model/users.js
+++ server/model/users.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+
 const userSchema = new mongoose.Schema({
-  name: { type: String },
-  email: { type: String },
-  age: { type: Number }
+    name: String,
+    email: String,
+    age: Number
 });
-const userModel = mongoose.model("users", userSchema);
-module.exports = userModel;
+
+module.exports = mongoose.model("student", userSchema);
